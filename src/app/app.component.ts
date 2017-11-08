957ab42452bdea43c350b3d4aaf87ef1e3479c6a
Angular CLI <angular-cli@angular.io>
2017-11-08 17:47:54 +0300

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'u8-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'u8';
+}
